bfb4fa43a5a8d652ba2d1fcc19ce605f1a83bc38
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-09-03 22:53:57 -0300

    .

--- CFB Cursos/Exercícios/objetos/objetos.cpp
+++ CFB Cursos/Exercícios/objetos/objetos.cpp
@@ -7,13 +7,13 @@ class dados                        //declaração de uma  classe
 {
 public:
     //declaração de atributos
-    vector <string> nome;
-    vector <string> genero;                      
-    vector <int> idade;
-    vector <float> peso;
+    string nome;
+    string genero;                      
+    int idade;
+    float peso;
 
     //declaração de métodos
-    void cadastrar(string nome, string genero, int idade);
+    void cadastrar(string n, string gener, int idd, float p);
     
 
 
@@ -21,13 +21,14 @@ public:
     //~dados();
 };
 
-
-void dados::cadastrar(string n, string gener, int idd)              //construção da classe
+void dados::cadastrar(string n, string gener, int idd, float p)              //construção da classe
 {
-    //this->nome.back(n);         //adiciona ao final do vector
-    //this->genero.back(gener);         //adiciona ao final do vector
-    //this->idade.back(idd);         //adiciona ao final do vector  
+    this->nome = n;         //adiciona ao final do vector
+    this->genero = gener;         //adiciona ao final do vector
+    this->idade = idd;         //adiciona ao final do vector  
+    this->peso = p;         //adiciona ao final do vector  
 }
+
 /*
 dados::~cadastrar()
 {
@@ -35,12 +36,32 @@ dados::~cadastrar()
 */
 
 int main()
-{   
+{  /*
+    vector <string> nome;
+    vector <string> genero;                      
+    vector <int> idade;
+    vector <float> peso;
+    */
+
+    string nome="", genero="";                    
+    int idade=0;
+    float peso=0;
+
     //declaração de objetos
-    //dados *cliente = new dados();               
+    dados *cliente = new dados();               
     //dados *filme = new dados();
 
-    //cliente->cadastrar();
+    cout << "Nome: ";     cin >> nome;
+    cout << "Genero: ";     cin >> genero;
+    cout << "Idade: ";     cin >> idade;
+    cout << "Peso: ";     cin >> peso;
+
+    cliente->cadastrar(nome, genero, idade, peso);
+
+    cout << "\nNome: " << cliente->nome;
+    cout << "\nGenero: " << cliente->genero;
+    cout << "\nIdade: " << cliente->idade;
+    cout << "\nPeso: " << cliente->peso <<"\n";
 
     return 0;
 }
