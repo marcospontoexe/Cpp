555a72a6c2005a4d0a32f768da7e16a4952d57ea
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-09-11 16:43:17 -0300

    .

--- CFB Cursos/Exercícios/manipulação de arquivos/fstream/fstream.cpp
+++ CFB Cursos/Exercícios/manipulação de arquivos/fstream/fstream.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 #include <fstream>            //biblioteca para manipular arquivos de dados
+#include <cstdlib>             //para usar system
 
 using namespace std;
 
+
 int main()
 {
+    fstream arquivo;
+    char continua = 's';
+    string temp, linha;
+
+    arquivo.open("dados.txt", ios::out);            //fstream funcionando como modo de saida (sai do programa e entra no arquivo)
     
+    while (continua == 's' or continua == 'S')
+    {
+        cout << "digite algo" << "\n";
+        cin >> temp;
+        arquivo << temp << " ";
+        cout << "continuar? [s/n]: " << "\n" ;
+        cin >> continua;
+        system("cls");
+        while(continua != 's' and continua != 'S' and continua != 'n' and continua != 'N')  
+        {
+            cout << "Valor incorreto!" <<"\n";
+            cout << "continuar? [s/n]: " << "\n" ;
+            cin >> continua;
+        }
+    } 
+    cout << "Programa finalizado!" << "\n";
+    arquivo.close();
+
+    arquivo.open("dados.txt", ios::in | ios::app);            //fstream funcionando como modo de etrada (sai do arquivo e entra no programa)
+    if(arquivo.is_open())        //verifica se o arquivo foi aberto
+   {
+       while(getline(arquivo, linha))        //lê linha por linha do arquivo
+       {
+           cout << linha << "\n";
+       }
+       arquivo.close();
+   }
+   else
+   {
+        cout << "erro ao abrir o arquivo";
+   }
     return 0;
 }
--- CFB Cursos/Exercícios/manipulação de arquivos/ifstream/ifstream.cpp
+++ CFB Cursos/Exercícios/manipulação de arquivos/ifstream/ifstream.cpp
@@ -5,6 +5,29 @@ using namespace std;
 
 int main()
 {
+    ofstream arquivo_out;           //objeto do tipo ofstream
+    ifstream arquivo_in;
+    string linha;
+
+    arquivo_out.open("dados.txt", ios::app);             //vincula o programa a um arquivo externo
+    arquivo_out << "Arquivo de teste" << "\n";          //armazena dentro do arquivo criado.
+    arquivo_out << "linha dois"<< "\n";          //armazena dentro do arquivo criado.
+    arquivo_out << "linha tres"<< "\n";          //armazena dentro do arquivo criado.
+    arquivo_out.close();                        //fecha o arquivo manipulado. Sempre que algo for aberto ou chamado, é importante finalizar apos o uso.    
+
+   arquivo_in.open("dados.txt");
+   if(arquivo_in.is_open())        //verifica se o arquivo foi aberto
+   {
+       while(getline(arquivo_in, linha))        //lê linha por linha do arquivo
+       {
+           cout << linha << "\n";
+       }
+       arquivo_in.close();
+   }
+   else
+   {
+        cout << "erro ao abrir o arquivo";
+   }
     
     return 0;
 }
