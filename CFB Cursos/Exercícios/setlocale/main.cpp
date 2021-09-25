2e144b6fae7e2f9771f30d9ef7a29f6546fd16e3
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-09-25 20:34:55 -0300

    .

--- CFB Cursos/Exercícios/setlocale/main.cpp
+++ CFB Cursos/Exercícios/setlocale/main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "portuguese");              //LC_ALL = locale all
+    //setlocale(LC_ALL, "portuguese.utf8");         //LC_ALL = locale all
+    //setlocale(LC_ALL,"pt_BR.UTF-8");
+    //setlocale(LC_ALL, "portuguese.utf8");              
+     setlocale(LC_ALL, "portuguese");
     cout << "agora é possível escrever maçã com acento!";   
 
     return 0;
