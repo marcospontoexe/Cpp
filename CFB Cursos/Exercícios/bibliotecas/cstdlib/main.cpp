3bd54d05bc1c6584e619f7cc090561c436db7fbc
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-09-26 12:54:18 -0300

    calloc e malloc

--- CFB Cursos/Exercícios/bibliotecas/cstdlib/main.cpp
+++ CFB Cursos/Exercícios/bibliotecas/cstdlib/main.cpp
@@ -9,17 +9,27 @@ int main()
     int num1, num2; 
     char tmp[10];
 
-    cout << "Digite um numero \n";
-    cin.get(tmp, 10);                   //as vezes o 'cin.get()' nao funciona se tem um 'cin >>' antes
-
-    num1 = atoi(tmp);                //alpha to int (atoi)
-    cout << "num1: " << num1 << "\n";
-
+    printf("Digite um numero \n");
+    gets(tmp);                   //as vezes o 'cin.get()' nao funciona se tem um 'cin >>' antes
+    //cin.get(tmp, 10);                   //as vezes o 'cin.get()' nao funciona se tem um 'cin >>' antes
+    //scanf("%c", &tmp);
+    num1 = atoi(tmp);                //alpha to int (atoi)    
     num2 = strtod(tmp, NULL);                //str to double 
-    cout << "num2: " << num2 << "\n";
+    printf("num1: %i | num2: %i\n", num1, num2);
 
-    srand(time(NULL));                                          //a cada chamada da função 'rand()', será gerada uma nova semente de geração de numeros aleatórios
-    cout << "Numero aleatorio: " << rand()%100 << "\n";         //gera um numero aleatório de 0 até 100
+
+    //alocação dinamica de memoria
+    int tamanho=10, n;
+    int *vetor;       //*vetor = vetor[4]
+    //vetor = (int*)calloc(tamanho, sizeof(int));           //alocando memoria para o vetor
+    vetor = (int*)malloc(sizeof(int));           //alocando memoria para o vetor, com o ponterio apontado para o primeiro elemento
+    srand(time(NULL));           //a cada chamada da função 'rand()', será gerada uma nova semente de geração de numeros aleatórios
+    for(int i = 0; i < tamanho; i++)
+    {
+        vetor[i] = rand()%100;
+        printf("vetor[%i]: %i\n", i,vetor[i]);          //gera um numero aleatório de 0 até 100
+    }
+    free(vetor);            //libera a memória que foi usada para o 'vetor'
 
     return 0;
 }
