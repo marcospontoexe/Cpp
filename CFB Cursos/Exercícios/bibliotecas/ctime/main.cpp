b42fee6074e42b13a40484368f16eeba43cae9d1
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-09-30 22:04:56 -0300

    ctime

--- CFB Cursos/Exercícios/bibliotecas/ctime/main.cpp
+++ CFB Cursos/Exercícios/bibliotecas/ctime/main.cpp
@@ -2,38 +2,25 @@
  #include <ctime>        //atualização da <time.h>
 
 using namespace std;
-/*
-struct dobrar
-{
-    int n;
-    int dobro()
-    {
-        return n*2;
-    }
-};
 
-int soma(int a, int b)
+void contador(int num, int cont = 0)
 {
-    return a+b;
+    cout << cont << "\n";
+    if(num > cont)
+    {
+        contador(num,  ++cont);
+    }
 }
-*/
+
 int main()
 {
-    auto n1{10}, n2{5};
-    cout << "numero_1: "<< n1 << " | numero_2: "<< n2 << "\n";
-    swap(n1, n2);
-    cout << "swap(n1, n2): " << "\n";
-    cout << "numero_1: "<< n1 << " | numero_2: "<< n2 << "\n";  
-
-    pair<int, int> p1;              //declaração de variáveis do tipo pair 
-    pair<int, char> p2;
-    pair<float,string>p3;
-    p1 = make_pair(1,5);            //adicionando um valor ao pair
-    p2=make_pair(10, 'A');              //char usa ' '
-    p3 = make_pair(55.6, "marcos");     //string usa " "
-    cout << "p1: " << p1.first << " | " << p1.second << "\n";
-    cout << "p2: " << p2.first << " | " << p2.second << "\n";
-    cout << "p3: " << p3.first << " | " << p3.second << "\n";
+    clock_t t;
+    t = clock();              //clock() retorna a quantidade de ciclos de processamento
+    cout << "Contagem: \n";
+    contador(100);
+    t = clock() - t;          //tempo atualizado é o tempo atual subtraido do tempo de processaemnto anterior
+    cout << "ciclos de processamento: " << t << "\n";
+    cout << "tempo de processamento: " << (float(t))/CLOCKS_PER_SEC << " s\n";
 
     
     return 0;
