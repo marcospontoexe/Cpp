38f2ae550ad99d3e4922b9b492ba29a42ecbcbae
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-10-02 14:18:04 -0300

    ctime

--- CFB Cursos/Exercícios/bibliotecas/ctime/main.cpp
+++ CFB Cursos/Exercícios/bibliotecas/ctime/main.cpp
@@ -15,6 +15,7 @@ void contador(int num, int cont = 0)
 int main()
 {
     clock_t t; 
+    size_t tamanho;             //recebe o tamanho da algo (uint)
     t = clock();              //clock() retorna a quantidade de ciclos de processamento
     cout << "Contagem: \n";
     contador(100);
@@ -23,10 +24,21 @@ int main()
     cout << "tempo de processamento: " << (float(t))/CLOCKS_PER_SEC << " s\n";
 
     time_t data;
+    char buf[30];
     struct tm *infodata;
-    time(&data);
-    infodata = localtime(&data);
-    cout << "hoda e data arual: " << asctime(infodata) << "\n";
+    time(&data);                                //associa o endereço do objeto 'data' à função time()
+    infodata = localtime(&data);                //converte o 'time_t' para a struct 'tm'
+    cout << "hoda e data atual: " << asctime(infodata) << "\n";     //converte o objeto 'infodata' para string
+    strftime(buf, 30, "Hora: %H:%M", infodata);             //a variável 'buf' recebe apenas a hora e minuto
+    cout << buf << "\n"; 
+    strftime(buf, 30, "Data: %d/%m/%Y", infodata);             //a variável 'buf' recebe apenas a data
+    cout << buf << "\n";    
+
+    time_t hora;
+    time(&hora);                                //associa o endereço do objeto 'data' à função time()
+    cout << "hoda e data atual: " << ctime(&hora) << "\n";     //com o ctime() não precisa converter para string
+
+
 
     return 0;
 }
