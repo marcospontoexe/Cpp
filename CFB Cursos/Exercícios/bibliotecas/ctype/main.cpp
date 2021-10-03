b0607836bc9af1eb2b3d834dfb568fda14fa7fa8
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-10-03 17:34:10 -0300

    cctype

---
+++ CFB Cursos/Exercícios/bibliotecas/ctype/main.cpp
@@ -0,0 +1,18 @@
+ #include <iostream>     //define o fluxo de entrada e saida de dados (cout, cin, cr)
+ #include <cctype>        //Evolução da bib <ctipy.h>. Possui funções que verificam dados de caracteres
+using namespace std;
+/*
+class teste
+{
+
+};
+*/
+int main()
+{
+     
+    char tmp[80]="Marcos Daniel Santana - 1990.";
+    
+    cout << "conteudo da tmp: \n"<< tmp << "\n";
+    
+    return 0;
+}
