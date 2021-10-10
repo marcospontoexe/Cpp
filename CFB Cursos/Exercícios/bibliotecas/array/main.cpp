d29d4601308668f33ab92dd781dbbb2479271b0d
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-10-10 13:49:00 -0300

    vector

--- CFB Cursos/Exercícios/bibliotecas/array/main.cpp
+++ CFB Cursos/Exercícios/bibliotecas/array/main.cpp
@@ -18,10 +18,25 @@ int main()
     {
         cout << *it <<"-";
     }
-    
-    
 
+    cout << "tamanho do array 'num': " << num.size() << "\n";               //tamanho do array
+    cout << "tamanho maximo do array 'num': " << num.max_size() << "\n";        //número maximo de elementos no array
+    if(num.empty())            //caso o array esteja vazio
+    {
+        cout << "array vazio!\n";
+    }
+
+    cout << "elemento da posicao 3: " << num[3] << "\n";   
+    cout << "elemento da posicao 0: " << num.front() << "\n";   //retorno o primeiro elemento do array       
+    cout << "ultimo elemento do array: " << num.back() << "\n";   //retorno o primeiro elemento do array 
     
+    num.fill(5);            //preenche todas as posições com numero 5
+    cout << "num:\n";
+    for (auto it = num.begin(); it != num.end(); it++)          //usando iterator
+    {
+        cout << *it <<"-";
+    }
+    cout << "\n";
 
 
 
