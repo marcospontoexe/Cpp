f9fe73ad48c55569798f42a745da298775236518
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-10-15 20:44:24 -0300

    cmath

---
+++ CFB Cursos/Exercícios/bibliotecas/cmath/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>     //define o fluxo de entrada e saida de dados (cout, cin, cr...)
+#include <cmath>      //biblioteca para usar funções matemáticas
+
+
+using namespace std;
+
+int main()
+{   
+
+    return 0;
+}
