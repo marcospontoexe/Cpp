0d2bdff4b9cea4ab4d01238e1ffa75dfd9af4adf
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-10-22 20:54:05 -0300

    formulario

--- CFB_Cursos/QT/formularios/objeto_local/formularios/mainwindow.cpp
+++ CFB_Cursos/QT/formularios/objeto_local/formularios/mainwindow.cpp
@@ -19,7 +19,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    janela form2;       //declaração do atributo (form2) da classe janela
+    janela form2;       //declaração do atributo da classe janela
 
 
     QMessageBox::StandardButton resposta;
