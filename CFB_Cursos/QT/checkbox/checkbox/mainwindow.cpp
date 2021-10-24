cc0f5a04851facf4898da101e2deddc23c99e04e
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-10-24 13:56:16 -0300

    checkbox

--- CFB_Cursos/QT/checkbox/checkbox/mainwindow.cpp
+++ CFB_Cursos/QT/checkbox/checkbox/mainwindow.cpp
@@ -28,15 +28,15 @@ void MainWindow::on_pushButton_clicked()        //botão selecionadas
     box_3 = ui->checkBox_3->isChecked();
     if (box_1)
     {
-        msg += " checkBox_1 selecionada";       //caso a checkBox_1 esteja selecionada
+        msg += " checkBox_1 selecionada.";       //caso a checkBox_1 esteja selecionada
     }
     if (box_2)
     {
-        msg += ", checkBox_2 selecionada";       //caso a checkBox_2 esteja selecionada
+        msg += "\ncheckBox_2 selecionada.";       //caso a checkBox_2 esteja selecionada
     }
     if (box_3)
     {
-        msg += ", checkBox_3 selecionada";       //caso a checkBox_3 esteja selecionada
+        msg += "\ncheckBox_3 selecionada.";       //caso a checkBox_3 esteja selecionada
     }
 
     QMessageBox::information(this, "cheekBoxes selecionadas", msg);
@@ -56,7 +56,7 @@ void MainWindow::on_pushButton_2_clicked()      //notão "2° maneira"
     {
         if(cx[i])
         {
-            msg += " checkBox_" + QString::number(i+1) + " selecionada";
+            msg += " checkBox_" + QString::number(i+1) + " selecionada.\n";
         }
     }
     QMessageBox::information(this, "cheekBoxes selecionadas", msg);
@@ -67,7 +67,7 @@ void MainWindow::on_checkBox_1_stateChanged(int arg1)         //arg1=1 caso a ca
 {
     if(arg1)
     {
-        m1 = " checkBox_1 marcada ";
+        m1 = " checkBox_1 marcada. ";
     }
     else
     {
@@ -76,20 +76,11 @@ void MainWindow::on_checkBox_1_stateChanged(int arg1)         //arg1=1 caso a ca
 }
 
 
-void MainWindow::on_pushButton_3_clicked()
-{
-    msg = m1 + m2 + m3;
-
-    QMessageBox::information(this, "cheekBoxes selecionadas", msg);
-
-}
-
-
 void MainWindow::on_checkBox_2_stateChanged(int arg1)
 {
     if(arg1)
     {
-        m2 = " checkBox_2 marcada ";
+        m2 = " checkBox_2 marcada. ";
     }
     else
     {
@@ -102,11 +93,20 @@ void MainWindow::on_checkBox_3_stateChanged(int arg1)
 {
     if(arg1)
     {
-        m3 = " checkBox_3 marcada ";
+        m3 = " checkBox_3 marcada. ";
     }
     else
     {
         m3 = "";
     }
 }
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    msg = m1 + "\n" + m2 + "\n" + m3;
+
+    QMessageBox::information(this, "cheekBoxes selecionadas", msg);
+
+}
+
 
