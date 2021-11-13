b9cd69d7be61c6d4811e6e607031c61bb4ab531e
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-11-13 13:31:54 -0300

    .

--- CFB_Cursos/QT/banco_de_dados/agenda/janelalogin.cpp
+++ CFB_Cursos/QT/banco_de_dados/agenda/janelalogin.cpp
@@ -1,13 +1,14 @@
 #include "janelalogin.h"
 #include "ui_janelalogin.h"
 
+static QSqlDatabase bancoDeDados = QSqlDatabase::addDatabase("QSQLITE");      //o objeto 'bancoDeDados' recebe o tipo de banco de dados a ser trabalhado (Sq lite)
+
+
 JanelaLogin::JanelaLogin(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::JanelaLogin)
 {
     ui->setupUi(this);
-
-    QSqlDatabase bancoDeDados = QSqlDatabase::addDatabase("QSQLITE");      //o objeto 'bancoDeDados' recebe o tipo de banco de dados a ser trabalhado (Sq lite)
     bancoDeDados.setDatabaseName("C:/Users/marco/Documents/GitHub/Repositorios/Cpp/CFB_Cursos/QT/banco_de_dados/data_base/logs.db");
     //.open(), abre o banco de deados
     if(!bancoDeDados.open())        //caso consiga abrir o banco de dados retorna true
@@ -28,9 +29,39 @@ JanelaLogin::~JanelaLogin()
 
 void JanelaLogin::on_pushButton_log_clicked()               //boão entrar
 {
+
     QString nome = ui->lineEdit_log->text();
     QString senha = ui->lineEdit_senha->text();
+    //.open(), abre o banco de deados
+    if(!bancoDeDados.open())        //caso consiga abrir o banco de dados retorna true
+    {
+        ui->textEdit->setText("Não foi possível abrir o banco de dados!");
+    }
 
+    QSqlQuery consuta;
+    if(consuta.exec("select * from logs where Log-in = ' " + nome + " ' and Senha = ' " + senha + " ' "))
+    {
+        ui->textEdit->setText("botaão apertado!");
+        int cont = 0;
+        while(consuta.next())
+        {
+            cont++;
+        }
+        if(cont > 0)
+        {
+            this->close();                       //fecha a janela_login
+            form_principal janela_principal;
+            janela_principal.setModal(true);
+            janela_principal.exec();
+        }
+        else
+        {
+            ui->textEdit->setText("Usuário e senha não encontrados!");
+            ui->lineEdit_log->clear();
+            ui->lineEdit_senha->clear();
+            ui->lineEdit_log->setFocus();
+        }
+    }
 
 }
 
